ca415f1c320058a8e03e6e8a39b8ef2e6cdd3969
MohammedZakhbat <zakhbat.med@gmail.com>
2026-05-11 10:39:09 +0200

    add tailwindcss

---
+++ app.js
@@ -0,0 +1,8 @@
+const tamagotchi = {
+    nombre :"Pataso",
+    salud : 8,
+    limpieza: 9,
+    energia:5,
+    personalidad :"Pataso es un patito que le gusta comer mucho, jugar, no le gusta que lo manden a dormir"
+}
+
